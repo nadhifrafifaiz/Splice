790fdef0a194852ffae394d1b295dc4ac34da114
Nadhif Rafifaiz K <nadhifrafifaiz@gmail.com>
2021-11-18 09:52:45 +0700

    [Slace] Setup redux action and reducer, toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux'
+import rootReducer from './redux/reducers/index'
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true
+})
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ client/src/redux/reducers/user.js
@@ -0,0 +1,17 @@
+const init_state = {
+    username: "",
+    email: "",
+    role: "",
+    id: "",
+}
+
+
+const reducer = (state = init_state, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+
+export default reducer
